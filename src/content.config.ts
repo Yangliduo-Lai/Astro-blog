376ed036abdb7e2e0070f2a03e89f018ed489472
Geometry <yangliduolai@gmail.com>
2025-11-23 12:11:31 +0800

    moments

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,8 @@
 import { glob } from "astro/loaders";
 // 从 `astro:content` 导入工具函数
 import { z, defineCollection } from "astro:content";
-// 为每个集合定义一个 `loader` 和 `schema`
+
+// Blog 集合
 const blog = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
     schema: z.object({
@@ -17,5 +18,17 @@ const blog = defineCollection({
       tags: z.array(z.string())
     })
 });
-// 导出一个单独的 `collections` 对象用以注册你的集合（们）
-export const collections = { blog };
+
+// Moments 集合
+const moments = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: "./src/pages/moments" }),
+  schema: z.object({
+    title: z.string().optional(),
+    date: z.date(),
+    images: z.array(z.string()).optional()
+  })
+});
+
+
+// 导出集合
+export const collections = { blog, moments };
